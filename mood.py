f6812c2bd783415246a49e510eed3de33078f79a
jaxtothemax <jax.atlet@hotmail.com>
2020-04-20 19:13:57 +0200

    nal_8.1

---
+++ mood.py
@@ -0,0 +1,16 @@
+user_mood = input("What mood are you in at the moment?")
+
+if user_mood == "happy":
+    print("It is great to see you happy!")
+elif user_mood == "nervous":
+    print("Take a deep breath 3 times.")
+elif user_mood == "sad":
+    print("Drink a few beers with your friends.")
+elif user_mood == "excited":
+    print("Scream YAHOOOOOO")
+elif user_mood == "relaxed":
+    print("Watch some Netflix")
+else:
+    print("I don't recognize this mood")
+
+    
